920473b2e7f6918dde18e2db5ed67919733a19f0
linkednode <linked.node09@gmail.com>
2025-03-02 23:40:13 +0700

    update the script

--- utils/autoupgrade/upgrade.sh
+++ utils/autoupgrade/upgrade.sh
@@ -18,6 +18,26 @@ done
 
 printLogo
 
+# Function to send messages to Telegram
+# If you want to receive Telegram notifications after updates, create a /root/tg.conf file with the following values:
+# CHAT_ID_ALARM=""
+# BOT_TOKEN=""
+if [ -f /root/tg.conf ]; then
+    source /root/tg.conf
+else
+    echo "Telegram configuration file not found. Telegram notifications will be disabled."
+fi
+send_telegram() {
+    local message=$1
+    if [ -n "$BOT_TOKEN" ] && [ -n "$CHAT_ID_ALARM" ]; then
+        local response=$(curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+            -d chat_id="$CHAT_ID_ALARM" -d text="$message" > /dev/null 2>&1)
+        echo "Sent Telegram message: $message"
+    else
+        echo "BOT_TOKEN or CHAT_ID_ALARM not set. Telegram message not sent."
+    fi
+}
+
 # Initialize variables for time calculations
 prev_time=$(date +%s)
 cur_time=0
@@ -107,6 +127,8 @@ for((;;)); do
     sudo systemctl restart ${BINARY}-test
     printLine
     echo -e "$GREEN Your node has been updated and restarted, the session will be terminated automatically after 15 min${NC}"
+    MESSAGE="$BINARY updated to ${VER} and service restarted"
+    send_telegram "$MESSAGE"
     echo "$(date): Your node successfully upgraded to ${VER}" >> $PROJECT_HOME/upgrade.log
     printLine
     break
